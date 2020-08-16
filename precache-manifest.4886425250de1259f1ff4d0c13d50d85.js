cda35b233fa662cca5d857678966a69b3e19eb41
jeswanthv <48980002+jeswanthv@users.noreply.github.com>
2020-08-16 10:59:07 +0530

    Updates

---
+++ precache-manifest.4886425250de1259f1ff4d0c13d50d85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb28a5f1e84dab98a7b7ef721c137e7c",
+    "url": "/VirusTracker-react/index.html"
+  },
+  {
+    "revision": "49fbbe8d90bad669d97b",
+    "url": "/VirusTracker-react/static/css/main.3da612a3.chunk.css"
+  },
+  {
+    "revision": "136fd02bf1e119304d88",
+    "url": "/VirusTracker-react/static/js/2.f8bf944b.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/VirusTracker-react/static/js/2.f8bf944b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49fbbe8d90bad669d97b",
+    "url": "/VirusTracker-react/static/js/main.02303c55.chunk.js"
+  },
+  {
+    "revision": "23c2570a17367f453217",
+    "url": "/VirusTracker-react/static/js/runtime-main.5e343422.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/VirusTracker-react/static/media/image.d7265326.png"
+  }
+]);
